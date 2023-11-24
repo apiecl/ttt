027c162a23cb3826e7840e81e207f28fa16bb1f3
Pablo Selín <pablo@apie.cl>
2023-11-25 00:02:52 +0100

    Low def version

--- src/TTTStage.tsx
+++ src/TTTStage.tsx
@@ -1,5 +1,4 @@
-import { Stage, Container, withFilters } from "@pixi/react";
-import * as PIXI from "pixi.js";
+import { Stage } from "@pixi/react";
 import { ReactNode } from "react";
 import { colors, size } from "./types/types";
 //import { BlurFilter, ColorMatrixFilter } from "pixi.js";
@@ -10,22 +9,13 @@ interface stageprops {
 }
 
 function TTTStage(props:stageprops) {
-  const Filters = withFilters(Container, {
-    blur: PIXI.BlurFilter,
-    noise: PIXI.NoiseFilter
-  });
   return (
     <Stage
       width={props.size.w}
       height={props.size.h}
       options={{ backgroundColor: colors.black, antialias: true }}
     >
-      <Filters
-        blur={{ blur: 5 }}
-        noise={{ noise: 0.2 }}
-      >
         {props.children}
-      </Filters>
     </Stage>
   );
 }
